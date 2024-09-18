a68360cff64ea2817c7f84da1c17df2864dcf4e3
Ricardo Palacios <rpmaya@gmail.com>
2024-09-18 18:07:11 +0200

    Update alturasDF.py

--- spark/alturasDF.py
+++ spark/alturasDF.py
@@ -7,13 +7,13 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 # Read CSV file from HDFS
-df = spark.read.csv("hdfs://namenode:9000/user/root/input/alturasRev.csv", header=False, inferSchema=True)
+dfo = spark.read.csv("hdfs://namenode:9000/user/root/input/alturasRev.csv", header=False, inferSchema=True)
 
 # Rename columns to 'sexo' and 'altura'
-df = df.withColumnRenamed("_c0", "sexo").withColumnRenamed("_c1", "altura")
+dfc = dfo.withColumnRenamed("_c0", "sexo").withColumnRenamed("_c1", "altura")
 
 # Convert 'altura' to float and correct heights that are in meters
-df = df.withColumn("altura", when(col("altura") < 10, col("altura") * 100).otherwise(col("altura")))
+df = dfc.withColumn("altura", when(col("altura") < 10, col("altura") * 100).otherwise(col("altura")))
 
 # Filter out invalid data (empty or negative heights)
 df_filtered = df.filter((col("sexo").isNotNull()) & (col("altura") > 0))
